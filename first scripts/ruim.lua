6036dcacee3d0ecc35d7565a975784ea7a7b06de
arroz097 <luizgabrielmatera@hotmail.com>
2026-02-12 23:08:06 -0300

    Create ruim.lua

---
+++ first scripts/ruim.lua
@@ -0,0 +1,30 @@
+---idk---
+-- lendario ruim...
+
+print("Made By Me fo43123")
+print("and nobody cares ik")
+
+---Music---
+
+local Plr = game.Players.LocalPlayer
+
+local a = Instance.new("Sound", Plr)
+
+a.Looped = true
+a.Pitch = 1
+a.Volume = 999999999999999999
+a.SoundId = "rbxassetid://151224226"
+a:Play()
+
+---Kaboom---
+
+while true do
+    local s = Instance.new("Part", game.Workspace)
+wait(0)
+s.Material = "Brick"
+s.Size = Vector3.new(750,750,750)
+s.Anchored = true
+s.BrickColor = BrickColor.random()
+end
+
+---End---
